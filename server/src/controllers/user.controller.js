480a1a1b06c082f30810772b6e02172872006ac1
HicaroD <hdanrlley1@gmail.com>
2023-01-15 19:40:57 -0300

    api(controller): creating `User` in database

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,5 +1,7 @@
-const create = (req, res) => {
-	const {name, username, email, password, avatar} = req.body;
+import userService from '../services/user.service.js';
+
+const create = async (req, res) => {
+	const { name, username, email, password, avatar } = req.body;
 
 	if (!name || !username || !email || !password || !avatar) {
 		res.status(400).send({
@@ -7,9 +9,18 @@ const create = (req, res) => {
 		});
 	}
 
+	const user = await userService.create(req.body);
+
+	if (!user) {
+		res.status(400).send({
+			message: "Unable to create user",
+		});
+	}
+
 	res.status(201).send({
 		message: "User created successfully",
 		user: {
+			id: user._id,
 			name,
 			username,
 			email,
