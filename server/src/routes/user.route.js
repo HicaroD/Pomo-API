38bcd958960d283a042d990b482c6c0f9162522b
HicaroD <hdanrlley1@gmail.com>
2023-01-15 20:52:38 -0300

    api(route): remove user

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -6,5 +6,6 @@ const router = Router();
 router.get("/", userController.findAll);
 router.post("/create", userController.create);
 router.patch("/update/:id", userController.update);
+router.delete("/remove/:id", userController.remove);
 
 export default router;
