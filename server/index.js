b9a2386f77752de98ec4b3a121d6c81b5e026cbd
HicaroD <hdanrlley1@gmail.com>
2023-01-15 17:50:43 -0300

    refactor: single instruction does not need `{}`

--- server/index.js
+++ server/index.js
@@ -6,6 +6,4 @@ const app = express();
 
 app.use("/user", userRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
