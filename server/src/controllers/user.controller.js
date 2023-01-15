bf011d87976d9f741c3931fad26e6e9c609c0b6a
HicaroD <hdanrlley1@gmail.com>
2023-01-15 18:19:03 -0300

    api: checking user passed all required fields

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,13 @@
 const create = (req, res) => {
-    const body = req.body;
-    res.json(body);
+    const { name, username, email, password, avatar } = req.body;
+
+    if(!name || !username || !email || !password || !avatar) {
+	res.status(400).send({
+	    message: "Send all required fields for registration",
+	});
+    }
+
+    res.json(req.body);
 };
 
 export default create;
