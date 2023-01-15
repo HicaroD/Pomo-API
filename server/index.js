593b9078a1a4c43eb098d193ea1b0052b81a1f3d
HicaroD <hdanrlley1@gmail.com>
2023-01-15 17:47:53 -0300

    proj: refactor folder source folder

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import userRouter from './src/routes/user.route.js';
 
+const PORT = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
+app.use("/user", userRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
