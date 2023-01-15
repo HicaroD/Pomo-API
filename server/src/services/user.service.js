84e6ca898265ad0c6f5876e44f70a327f64d84f8
HicaroD <hdanrlley1@gmail.com>
2023-01-15 20:31:22 -0300

    api(service): `findAll` and `findById`

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -1,5 +1,21 @@
 import User from '../models/User.js';
 
+const findAll = () => User.find();
+
 const create = (body) => User.create(body);
 
-export default { create };
+const findById = (id) => User.findById(id);
+
+const update = (
+    id,
+    name,
+    username,
+    email,
+    password,
+    avatar
+) => User.findOneAndUpdate(
+    {_id: id},
+    {name, username, email, password, avatar}
+);
+
+export default {create, update, findById, findAll};
