8a5d228aefebc72bd2e0d2b62af7a95baed13cdb
HicaroD <hdanrlley1@gmail.com>
2023-01-16 13:18:46 -0300

    server(auth/controller): generating JWT on login

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -7,12 +7,13 @@ const login = async (req, res) => {
 
 	const user = await authService.findByEmail(email);
 	const isPasswordValid = await bcrypt.compare(password, user.password);
+	const token = authService.generateToken(user.id);
 
 	if(!isPasswordValid || !user) {
 	    return res.status(400).send({message: "E-mail or password is not valid"});
 	}
 
-	return res.send({message: "Login was made successfully"});
+	return res.send({message: "Login was made successfully", token});
     } catch (error) {
 	return res.status(500).send({ message: error.message });
     }
--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,5 +1,11 @@
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const generateToken = (id) => jwt.sign({ id }, process.env.JWT_PRIVATE_KEY, { expiresIn: 86400});
 
 const findByEmail = (email) => User.findOne({ email: email }).select("+password");
 
-export default { findByEmail };
+export default { findByEmail, generateToken };
