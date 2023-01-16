1dd6288f9c166b06f7956758297b7c07c9352e47
HicaroD <hdanrlley1@gmail.com>
2023-01-16 10:47:17 -0300

    api(controller): refactoring controllers with middlewares

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,26 +1,23 @@
-import mongoose from 'mongoose';
 import userService from '../services/user.service.js';
 
 const create = async (req, res) => {
     const { name, username, email, password, avatar } = req.body;
 
     if (!name || !username || !email || !password || !avatar) {
-	res.status(400).send({
+	return res.status(400).send({
 	    message: "send all required fields for registration",
 	});
-	return;
     }
 
     const user = await userService.create(req.body);
 
     if (!user) {
-	res.status(400).send({
+	return res.status(400).send({
 	    message: "Unable to create user",
 	});
-	return;
     }
 
-    res.status(201).send({
+    return res.status(201).send({
 	message: "User created successfully",
 	user: {
 	    id: user._id,
@@ -36,69 +33,41 @@ const update = async (req, res) => {
     const { name, username, email, password, avatar } = req.body;
 
     if (!name && !username && !email && !password && !avatar) {
-	res.status(400).send({
+	return res.status(400).send({
 	    message: "Send at least one field for updating the user",
 	});
-	return;
     }
 
     const id = req.params.id;
 
-    if(!(mongoose.Types.ObjectId.isValid(id))) {
-	res.status(400).send({
-	    message: "Invalid user ID",
-	});
-	return;
-    }
-
-    const user = await userService.findById(id);
-
-    if(!user) {
-	res.status(400).send({
-	    message: "User not found",
-	});
-	return;
-    }
-
     await userService.update(id, name, username, email, password, avatar);
-    res.send({message: "User updated"});
+    return res.send({message: "User updated"});
 };
 
 const findAll = async (_, res) => {
     const users = await userService.findAll();
 
     if(users.length === 0) {
-	res.status(400).send({
+	return res.status(400).send({
 	    message: "No users found",
 	});
-	return;
     }
 
-    res.send(users);
+    return res.send(users);
+};
+
+const findById = async (req, res) => {
+    const id = req.params.id;
+    const user = await userService.findById(id);
+    return res.send(user);
 };
 
 const remove = async (req, res) => {
     const id = req.params.id;
 
-    if(!(mongoose.Types.ObjectId.isValid(id))) {
-	res.status(400).send({
-	    message: "Invalid user ID",
-	});
-	return;
-    }
-
-    const user = await userService.findById(id);
-
-    if(!user) {
-	res.status(400).send({
-	    message: "User does not exist",
-	});
-	return;
-    }
-
     await userService.removeById(id);
 
-    res.send({ message: "User deleted successfully" });
+    return res.send({ message: "User deleted successfully" });
 };
 
-export default { create, update, findAll, remove };
+export default { create, update, findAll, remove, findById };
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,11 +1,13 @@
 import {Router} from 'express';
 import userController from '../controllers/user.controller.js';
+import globalMiddlewares from '../middlewares/global.middlewares.js';
 
 const router = Router();
 
 router.get("/", userController.findAll);
 router.post("/create", userController.create);
-router.patch("/update/:id", userController.update);
-router.delete("/remove/:id", userController.remove);
+router.get("/:id", globalMiddlewares.isValidId, globalMiddlewares.isValidUser, userController.findById);
+router.patch("/update/:id", globalMiddlewares.isValidId, globalMiddlewares.isValidUser, userController.update);
+router.delete("/remove/:id", globalMiddlewares.isValidId, globalMiddlewares.isValidUser, userController.remove);
 
 export default router;
