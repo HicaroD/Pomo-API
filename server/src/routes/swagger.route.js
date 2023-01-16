c6ea8e13be8aaa76b5141726458a8a370003f45b
HicaroD <hdanrlley1@gmail.com>
2023-01-16 20:38:39 -0300

    api(fix): CJS not recognized

--- server/src/routes/swagger.route.js
+++ server/src/routes/swagger.route.js
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger.json' assert { type: "json" };
+const swaggerDocument = require('../swagger.json');
 
 const router = Router();
 
