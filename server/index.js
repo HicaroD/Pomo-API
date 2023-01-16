afc7b1ad2f05e69bbae319200d5823859092703b
HicaroD <hdanrlley1@gmail.com>
2023-01-16 20:13:11 -0300

    api(docs): adding swagger routes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import connectDatabase from './src/database/database.js';
 
 import authRouter from './src/routes/auth.route.js';
+import swaggerRoute from './src/routes/swagger.route.js';
 import userRouter from './src/routes/user.route.js';
 
 dotenv.config();
@@ -17,5 +18,6 @@ app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
+app.use("/docs", swaggerRoute);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,4 +1,6 @@
-import {Router} from 'express';
+import {
+    Router
+} from 'express';
 import userController from '../controllers/user.controller.js';
 import globalMiddlewares from '../middlewares/global.middlewares.js';
 import authMiddleware from '../middlewares/auth.middlewares.js';
@@ -7,9 +9,9 @@ const router = Router();
 
 // TODO: format this code
 router.get("/", userController.findAll);
-router.post("/create", userController.create);
-router.get("/:id", globalMiddlewares.isValidId, globalMiddlewares.isValidUser, userController.findById);
-router.patch("/update/:id", authMiddleware.auth, globalMiddlewares.isValidId, globalMiddlewares.isValidUser, userController.update);
-router.delete("/remove/:id", authMiddleware.auth, globalMiddlewares.isValidId, globalMiddlewares.isValidUser, userController.remove);
+router.post("/create/", userController.create);
+router.get("/:id/", globalMiddlewares.isValidId, globalMiddlewares.isValidUser, userController.findById);
+router.patch("/update/:id/", authMiddleware.auth, globalMiddlewares.isValidId, globalMiddlewares.isValidUser, userController.update);
+router.delete("/remove/:id/", authMiddleware.auth, globalMiddlewares.isValidId, globalMiddlewares.isValidUser, userController.remove);
 
 export default router;
