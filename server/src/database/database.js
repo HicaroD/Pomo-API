a8c7e3314afa0e2ec2caf2095a7b2ace584ec43e
HicaroD <hdanrlley1@gmail.com>
2023-01-15 21:01:57 -0300

    server(database): adding MongoDB database connection

---
+++ server/src/database/database.js
@@ -0,0 +1,19 @@
+import moongose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+moongose.set('strictQuery', false);
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const connectDatabase = () => {
+    moongose.connect(
+	MONGODB_URL,
+	{useNewUrlParser: true, useUnifiedTopology: true},
+    )
+    .then(() => console.log("Connected to MongoDB database"))
+    .catch((err) => console.log(err));
+};
+
+export default connectDatabase;
