81a74f41c08345f017f1a7142b066869be64c94c
HicaroD <hdanrlley1@gmail.com>
2023-01-23 10:52:59 -0300

    server: fmt files

--- server/src/middlewares/auth.middlewares.js
+++ server/src/middlewares/auth.middlewares.js
@@ -5,39 +5,39 @@ import userService from '../services/user.service.js';
 dotenv.config();
 
 const auth = (req, res, next) => {
-    try {
-	const { authorization } = req.headers;
+	try {
+		const { authorization } = req.headers;
 
-	if (!authorization) {
-	    return res.sendStatus(401)
+		if (!authorization) {
+			return res.sendStatus(401)
+		}
+		const parts = authorization.split(" ");
+
+		if (parts.length !== 2) {
+			return res.sendStatus(401);
+		}
+		const [schema, jwtToken] = parts;
+
+		if (schema !== "Bearer") {
+			return res.sendStatus(401);
+		}
+
+		jwt.verify(jwtToken, process.env.JWT_PRIVATE_KEY, async (error, decoded) => {
+			if (error) {
+				return res.status(401).send({ message: "Invalid token" });
+			}
+
+			const user = await userService.findById(decoded.id);
+
+			if (!user) {
+				return res.status(401).send({ message: "Invalid token" });
+			}
+			req.userId = decoded._id;
+			return next();
+		});
+	} catch (error) {
+		return res.status(500).send({ message: error.message });
 	}
-	const parts = authorization.split(" ");
-
-	if (parts.length !== 2) {
-	    return res.sendStatus(401);
-	}
-	const [schema, jwtToken] = parts; 
-
-	if(schema !== "Bearer") {
-	    return res.sendStatus(401);
-	}
-
-	jwt.verify(jwtToken, process.env.JWT_PRIVATE_KEY, async (error, decoded) => {
-	    if(error) {
-		return res.status(401).send({ message: "Invalid token" });
-	    }
-
-	    const user = await userService.findById(decoded.id);
-
-	    if(!user) {
-		return res.status(401).send({ message: "Invalid token" });
-	    }
-	    req.userId = decoded._id;
-	    return next();
-	});
-    } catch (error) {
-	return res.status(500).send({ message: error.message });
-    }
 };
 
 export default { auth };
